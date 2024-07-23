e9cfe2115820ef6b0ab39bb02bc64eee5e8d70a6
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-07-23 04:24:58 +0000

    feat: public bulkapis(wip)

--- packages/nocodb/src/controllers/public-datas.controller.ts
+++ packages/nocodb/src/controllers/public-datas.controller.ts
@@ -189,4 +189,34 @@ export class PublicDatasController {
     );
     return paginatedResponse;
   }
+
+  @Get(['/api/v2/public/shared-view/:sharedViewUuid/bulk/dataList'])
+  async bulkDataList(
+    @TenantContext() context: NcContext,
+    @Req() req: NcRequest,
+    @Param('sharedViewUuid') sharedViewUuid: string,
+  ) {
+    const response = await this.publicDatasService.bulkDataList(context, {
+      query: req.query,
+      password: req.headers?.['xc-password'] as string,
+      sharedViewUuid,
+    });
+
+    return response;
+  }
+
+  @Get(['/api/v2/public/shared-view/:sharedViewUuid/bulk/group'])
+  async bulkGroupBy(
+    @TenantContext() context: NcContext,
+    @Req() req: NcRequest,
+    @Param('sharedViewUuid') sharedViewUuid: string,
+  ) {
+    const response = await this.publicDatasService.bulkGroupBy(context, {
+      query: req.query,
+      password: req.headers?.['xc-password'] as string,
+      sharedViewUuid,
+    });
+
+    return response;
+  }
 }
